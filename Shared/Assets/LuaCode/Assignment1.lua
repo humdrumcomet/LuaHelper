1bb127ad633956023ea499fa08e37faad7c26010
Aaron English <humdrumcomet@protonmail.com>
2020-11-21 14:14:23 -0500

    restructured for use as actual package (via usepackage) and cleaner access as well as keeping track of required packages

--- Shared/Assets/LuaCode/Assignment1.lua
+++
@@ -1,4 +0,0 @@
------------------------------Q1b)
-function etchDimA(w,t) return (w-(2*t/math.tan(math.pi*54.7/180))) end
------------------------------Q1c)
-function etchTime(rate,tin,time) return tin-rate*time end

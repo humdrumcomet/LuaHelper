22877b9e5e97a2e5c8e66bc4034034be28a22b33
Aaron English <humdrumcomet@protonmail.com>
2020-12-10 10:01:42 -0500

    added ability to interpret list and quotes in the opts expansion. working on finishing the printluatable functions. missing layering options

--- luaHelper.lua
+++ luaHelper.lua
@@ -8,13 +8,28 @@ function printNum(num, reduce)
     end
 end
 function printTable(tableItems, modifiers)
-    prepend = modifiers.prepend or ''
-    append = modifiers.append or ''
-    separator = modifier.separator or ''
-    dir = modifier.dir or ''
-    slice = modifier.slice or ''
+    print('----------in print')
+    modifier.curDepth = modifiers.curDepth+1 or 1
+    curDepth = modifiers.curDepth
+    pre= modifiers.pre or ''
+    app= modifiers.app or ''
+    sep= modifiers.sep or ''
+    slice = modifiers.slice or ''
+    depth = modifiers.depth-1 or 0
+    modifiers.depth = depth
+    align= modifiers.align or 'vert'
     for i in tableItems do
-        print('test')
+        tex.sprint(prepend)
+        if depth>0 then
+            printTable(i, modifiers)
+        else
+            tex.sprint(i)
+        end
+        tex.print(append)
+        if align == 'vert' then
+            tex.print('')
+        end
+        --print('test')
         --if then
         --  tex.print()
         --  tex.sprint()
@@ -26,9 +41,31 @@ function optsToTable(opts)
     if opts == 'empty' then
         return ''
     end
+    interTbl = {}
     inputs = {}
-    for set in string.gmatch(opts, '([^,]+)') do
-        for key, value in string.gmatch(set, '(%w+)%s*=%s*(.*),*') do
+    quotePat = "(%w+)%s*=%s*(%b''),*"
+    quoteRm = "%w+%s*=%s*%b'',*"
+    brackPat = '(%w+)%s*=%s*(%b{}),*'
+    brackRm = '%w+%s*=%s*%b{},*'
+    commaPat = '([^,]+)'
+    equalsPat = '(%w+)%s*=%s*(.*),*'
+    for key, value in string.gmatch(opts, quotePat) do
+        inter = string.gsub(value, "'$", "")
+        item = string.gsub(inter, "^'", "")
+        inputs[key] = item
+    end
+    for key, value in string.gmatch(opts, brackPat) do
+        inputs[key] = {}
+        inter = string.gsub(value, "}$", "")
+        item = string.gsub(inter, "^{", "")
+        for set in string.gmatch(item, commaPat) do
+            table.insert(inputs[key], set)
+        end
+    end
+    inter = string.gsub(opts, quoteRm, '')
+    item = string.gsub(inter, brackRm, '')
+    for set in string.gmatch(item, commaPat) do
+        for key, value in string.gmatch(set, equalsPat) do
             inputs[key] = value
         end
     end
